b2b58bb50e3d907e2072e320ec117e4bcd7f47ad
Tommaso Figlia <tommasofiglia@gmail.com>
2020-11-17 15:21:31 +0100

    Inserito il nome che appare sul biglietto clickando genera

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
-var inputName = document.getElementById("full-name");
-var inputKilometers = document.getElementById("kilometers");
-var inputAge = document.getElementById("age-range");
+// Definisco le variabili che contengono i due button, genera e annulla.
+
+var actionGenera = document.getElementById('btn-genera');
+var actionAnnulla = document.getElementById('btn-annulla');
+
+// Click su genera
+
+actionGenera.addEventListener('click', function () {
+
+    // Descrivo le variabili relative a Nome, km e range di età inseriti
+    var inputKilometers = document.getElementById('kilometers');
+    var inputFullName = document.getElementById('full-name');
+    var inputAge = document.getElementById('age-range');
+
+    // Descrivo le variabili relative ai value assunti da Nome, km e range di età inseriti
+    var inputKilometersValue = inputKilometers.value;
+    var inputFullNameValue = inputFullName.value;
+    var inputAgeValue = inputAge.value;
+
+    // Scrivo il nome nel biglietto
+    document.getElementById('nome-passeggero').innerHTML = inputFullNameValue ;
+
+
+  }
+);
